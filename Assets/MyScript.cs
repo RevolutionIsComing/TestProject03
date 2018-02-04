645b7b49790efcf5af5fddbda1d04af1339a1829
DESKTOP-KL854F3\User <krsgedutis@gmail.com>
2018-02-04 12:56:35 +0000

    no message

---
+++ Assets/MyScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("You found that you are smart");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
